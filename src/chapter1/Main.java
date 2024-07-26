b84c50999797baff6bac0f5c9f53e5740a5c2b6a
jongchan-hong <jongchan988@gmail.com>
2024-07-27 00:23:34 +0900

    Add chapter1 only public

---
+++ src/chapter1/Main.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+// chapter 1 public
+// public 만을 사용하여 동작하는 과정을 보여주기 위한 코드를 작성함
+public class Main {
+    public static void main(String[] args) {
+        People student = new People("홍길동");
+        System.out.println(student.name);
+    }
+}
---
+++ src/chapter1/People.java
@@ -0,0 +1,9 @@
+package chapter1;
+
+public class People {
+    public String name;
+
+    public People(String name){
+        this.name = name;
+    }
+}
